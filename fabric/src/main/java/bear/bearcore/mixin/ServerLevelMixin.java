2a528321e3fad77c63ce833c585778c887d7b1d6
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-05-16 19:45:37 -0400

    added fabric side

---
+++ fabric/src/main/java/bear/bearcore/mixin/ServerLevelMixin.java
@@ -0,0 +1,15 @@
+package bear.bearcore.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.server.level.ServerLevel;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ServerLevel.class)
+public class ServerLevelMixin {
+    @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;setDayTime(J)V"))
+    private void wrapDaySet(ServerLevel instance, long l, Operation<Void> original){
+        original.call(instance, l + 1000);
+    }
+}
